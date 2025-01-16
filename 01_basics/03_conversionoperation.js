19f1695439dceeef9f058f278bdd170b8fe90c2a
sagar-prorigo <sagar.negi@prorigosoftware.com>
2025-01-16 09:13:39 +0000

    conversion operations completed

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,69 @@
+let num = "9823ads"
+
+console.log(typeof num)
+console.log(typeof (num))
+
+let IsNumber =Number(num);
+ console.log(typeof IsNumber) //output =>number
+
+ console.log(IsNumber)  //NAN i.e not a number 
+
+ //'33' => 33;
+ //"33abc" => NAN ;
+ // true => 1; false =0;
+ /*
+
+ let isLoggedIn = "samar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "samar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+ 1=true; 0 = false;
+ "" = false;
+ "Sagar" = true;
+ */
+
+ // *********************** Operations ***********************
+
+ let val =10;
+  let negativeval= -val;
+  console.log(negativeval);
+
+  let str1 = "Sagar"
+
+  let str2 = " Is a software developer "
+
+  let str3=str1+str2;
+
+  console.log(str3);
+
+  console.log("1" + 2 +3)
+
+  console.log(1+"2")
+
+  console.log(1+2+"3")
+
+  console.log("1"+2+3)
+
+  console.log(true)
+  console.log(+true)
+
+  let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
